54d302e599044726844a892f4f5ead186daa5739
und3rdg <und3rdg@gmail.com>
2016-07-11 16:31:40 +0100

    Add option to check ip from google, change external to public

--- myip.py
+++ myip.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
-# GETING EXTERNAL IP FROM DYNDNS.ORG
+# GETING PUBLIC IP FROM DYNDNS.ORG
 import urllib.request
 from re import findall
-def extIp():
-    url = 'http://checkip.dyndns.org'
-    regexp = '<body>Current IP Address: (.*?)</body>'
+def extIp(site):
+    ipMask = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    if site == 'dyndns':
+        url = 'http://checkip.dyndns.org'
+        regexp = '<body>Current IP Address: ('+ipMask+')</body>'
+    if site == 'google':
+        url = 'https://www.google.co.uk/search?q=check+ip'
+        regexp = '<!--m--><div.*?><div><div class=.*?>('+ipMask+').*?Your public IP address</div></div></div><!--n--></div>'
 
+    req = urllib.request.Request(
+        url,
+        data=None,
+        headers={
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'    
+        }
+    )
     try:
-        opener = urllib.request.urlopen(url,timeout=10)
+        # opener = urllib.request.urlopen(url,timeout=10)
+        opener = urllib.request.urlopen(req,timeout=10)
     except urllib.error.URLError as e:
         print(e.reason)
     html = opener.read()
@@ -27,11 +40,12 @@ def localIp():
 # CLI ARGUMENTS
 import argparse
 import sys
-def options():
+def flags():
     parser = argparse.ArgumentParser()
     
     parser.add_argument('-l', '--local', help='show local ip', action='store_true')
-    parser.add_argument('-e', '--external', help='show external ip', action='store_true')
+    parser.add_argument('-p', '--public', help='show public ip', action='store_true')
+    parser.add_argument('-g', '--google', help='use google to check ip, faster but may block ip', action='store_true')
     parser.add_argument('-v', '--verbose', help='Make output verbose', action='store_true')
 
     args = parser.parse_args()
@@ -39,16 +53,23 @@ def options():
     if len(sys.argv) < 2:
         parser.print_help()
         print('\nYour local IP:', localIp())
-        print('Your external IP:', extIp())
+        print('Your public IP:', extIp())
 
     if args.local:
         if args.verbose:
-            print('Your local IP:', localIp())
+            print('system => Your local IP:', localIp())
         else:
             print(localIp())
-    if args.external:
+
+    if args.google:
+        site = 'google'
+        args.public = True
+    else:
+        site = 'dyndns'
+
+    if args.public:
         if args.verbose:
-            print('Your external IP:', extIp())
+            print(site, '=> Your public IP:', extIp(site))
         else:
-            print(extIp())
-options()
+            print(extIp(site))
+flags()
